265e602501ac3a2bee4b9a46e1f0588706df3e79
alfixed <alfixed@gmail.com>
2017-10-16 19:38:59 +0300

    First refactoring

--- js/app.js
+++ js/app.js
@@ -1,7 +1,7 @@
 var my_news = [
   {
     author: 'Саша Печкин',
-    text: 'В четверг, четвертого числа...'
+    text: 'В четчерг, четвертого числа...'
   },
   {
     author: 'Просто Вася',
@@ -13,6 +13,20 @@ var my_news = [
   }
 ];
 
+var Article = React.createClass({
+  render: function() {
+    var author = this.props.data.author,
+        text = this.props.data.text;
+
+    return (
+      <div className='article'>
+        <p className='news__author'>{author}:</p>
+        <p className='news__text'>{text}</p>
+      </div>
+    )
+  }
+});
+
 var News = React.createClass({
   render: function() {
     var data = this.props.data;
@@ -22,29 +36,18 @@ var News = React.createClass({
       newsTemplate = data.map(function(item, index) {
         return (
           <div key={index}>
-            <p className="news__author">{item.author}:</p>
-            <p className="news__text">{item.text}</p>
+            <Article data={item} />
           </div>
         )
       })
     } else {
       newsTemplate = <p>К сожалению новостей нет</p>
     }
-    
-    return (
-      <div className="news">
-        {newsTemplate}
-        <strong className={data.length > 0 ? '':'none'}>Всего новостей: {data.length}</strong>
-      </div>
-    );
-  }
-});
 
-var Comments = React.createClass({
-  render: function() {
     return (
-      <div className="comments">
-        Нет новостей - комментировать нечего.
+      <div className='news'>
+        {newsTemplate}
+        <strong className={'news__count ' + (data.length > 0 ? '':'none') }>Всего новостей: {data.length}</strong>
       </div>
     );
   }
@@ -53,10 +56,9 @@ var Comments = React.createClass({
 var App = React.createClass({
   render: function() {
     return (
-      <div className="app">
-        Всем привет, я компонент App! Я умею отображать новости.
-        <News data={my_news} /> {/*добавили свойство data */}
-        <Comments />
+      <div className='app'>
+        <h3>Новости</h3>
+        <News data={my_news} />
       </div>
     );
   }
